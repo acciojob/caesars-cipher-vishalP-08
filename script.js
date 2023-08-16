a3ad771128d085339955d90e31b900691d177b0d
Vishal.AI <132605163+vishalP-08@users.noreply.github.com>
2023-08-16 17:11:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -32,10 +32,11 @@ const lookup = {
 };
 
 function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-
-  return; //return decodedArr
+  return encodedString.replace(/[A-Z]/g, char => {
+    const charCode = char.charCodeAt(0);
+    const shiftedCode = charCode >= 78 ? charCode - 13 : charCode + 13;
+    return String.fromCharCode(shiftedCode);
+  });
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
